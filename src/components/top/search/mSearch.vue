248cebcbb39ab60b682010003e946dfce0c4364b
jeff <jeff-hui@qq.com>
2024-04-04 09:59:39 +0800

    fix:搜索建议高亮

--- src/components/top/search/mSearch.vue
+++ src/components/top/search/mSearch.vue
@@ -11,8 +11,14 @@ let data = ref<searchSuggest>()
 let show = ref(false)
 let ls = ref<Hot>({} as unknown as Hot)
 async function search(keywords: string) {
-  show.value = !show.value
   data.value = await SearchApi.Suggest(keywords)
+
+  data.value.result.songs.forEach((item) => {
+    // 正则匹配item.name 与keyword,匹配到的字符使用span标签包裹
+    return (item.name = item.name.replace(new RegExp(keywords, 'g'), (match) => {
+      return `<span class='text-sky-500'>${match}</span>` //为什么没有被渲染
+    }))
+  })
 }
 
 async function enterSearch(keywords: string) {
@@ -54,13 +60,11 @@ onMounted(async () => {
 onUnmounted(() => {
   clearInterval(timer!)
 })
-//TODO 搜索建议关键词匹配
-//TODO 搜索无建议时央视
 </script>
 
 <template>
-  <div class="w-full relative flex max-md:ml-0">
-    <i class="fa-solid fa-magnifying-glass absolute top-[25%] left-2 opacity-50"></i>
+  <div class="w-full relative flex max-md:ml-0 text-xs">
+    <i class="fa-solid fa-magnifying-glass absolute top-[23%] left-3 opacity-50 text-sm"></i>
     <input
       type="text"
       class="dark:bg-gray-800 transition-all duration-700 w-52 focus:w-64 rounded-3xl border caret-pink-500 pl-8 py-2 max-sm:py-1 text-xs text-gray-600 focus:outline-sky-200 hover:border-lime-300 outline-none"
@@ -73,15 +77,16 @@ onUnmounted(() => {
       @keyup.enter="enterSearch(keyword ? keyword : Text)" />
     <div
       v-show="show"
-      class="flex max-sm:scale-90 suggest min-h-[13rem] overflow-hidden absolute top-[120%] max-sm:top-2 max-sm:-left-2 z-20 text-sm w-72 bg-white dark:bg-gray-800 rounded-md shadow-lg">
-      <div v-if="data?.result" class="h-full w-full">
+      class="flex max-sm:scale-90 suggest min-h-[15rem] absolute top-[120%] max-sm:top-2 max-sm:-left-2 z-20 w-72 bg-white dark:bg-gray-800 rounded-md shadow-lg">
+      <div v-if="data?.result" class="h-full w-full absolute">
         <div v-if="data.result.albums" class="flex flex-col gap-2 p-2 h-full border dark:bg-gray-800 bg-white">
           <div class="flex p-1 border-b">
             <h2 class="self-center w-2/12 flex-shrink-0 text-gray-500 text-center">单曲</h2>
             <div class="flex-1 w-10/12">
-              <div class="flex gap-2 flex-wrap text-sm" v-for="(item, index) in data.result.songs" :index="index">
+              <div class="flex gap-2 flex-wrap" v-for="(item, index) in data.result.songs" :index="index">
                 <p class="py-2 w-full hover:bg-gray-100 pl-2 truncate" @click="enterSearch(item.name)">
-                  {{ item.name }} - {{ item.artists.map((i) => i.name).join('、') }}
+                  <span v-html="item.name"></span>
+                  {{ item.artists.map((i) => i.name).join('、') }}
                 </p>
               </div>
             </div>
@@ -89,7 +94,7 @@ onUnmounted(() => {
           <div class="flex p-1 border-b">
             <h2 class="self-center w-2/12 text-gray-500 text-center">专辑</h2>
             <div class="flex-1 w-10/12">
-              <div class="flex gap-2 flex-wrap text-sm" v-for="(item, index) in data.result.albums" :index="index">
+              <div class="flex gap-2 flex-wrap" v-for="(item, index) in data.result.albums" :index="index">
                 <p class="py-2 w-full hover:bg-gray-100 pl-2 truncate" @click="enterSearch(item.name)">
                   {{ item.name }} - {{ item.artist.name }}
                 </p>
@@ -97,9 +102,9 @@ onUnmounted(() => {
             </div>
           </div>
         </div>
-        <template v-else>
-          <div class="h-full border-none justify-center items-center flex">没有搜到歌曲哦~</div>
-        </template>
+        <div v-else class="h-full border-none justify-center items-center flex">
+          <ElEmpty :description="'没有搜到歌曲哦~'" class="text-xs" :image-size="70"></ElEmpty>
+        </div>
       </div>
 
       <div v-else class="w-full">
