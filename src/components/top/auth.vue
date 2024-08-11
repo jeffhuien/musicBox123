bd5bb8fc7af49c67849a49848ecf37270fcc6fb4
jeff <88653028+jeffhuien@users.noreply.github.com>
2024-08-11 22:26:36 +0800

    fix: 修复读取用户信息失败

--- src/components/top/auth.vue
+++ src/components/top/auth.vue
@@ -14,11 +14,12 @@ function aa() {
 <template>
   <div class="auth" @click="aa">
     <div class="flex items-center gap-2">
-      <el-avatar :src="user ? user?.data.profile.avatarUrl : 'img/logo.png'" />
+      <el-avatar :src="user ? user?.data?.profile?.avatarUrl : 'img/logo.png'" />
       <div class="opacity-50">
-        <p class="text-xs">{{ user ? user.data.profile.nickname : '登录' }}</p>
+        <p class="text-xs">{{ user ? user.data?.profile?.nickname : '登录' }}</p>
         <!-- <p class="bg-sky-500 h-[.2rem] scale-75" /> -->
-        <p class="rounded-xl border-2 px-2 -translate-x-1 text-xs scale-75 text-white bg-pink-600">
+        <p
+          class="rounded-xl border-2 px-2 -translate-x-1 text-xs scale-75 text-white bg-pink-600">
           Lv.{{ user ? level : '0' }}
         </p>
       </div>
